9e7cf21246623b414c16a05dd6cbc219a3400f8c
shuna1907 <cetinsumeyra03@gmail.com>
2025-12-20 16:32:33 +0300

    kitap listeleme eklendi

--- main.py
+++ main.py
@@ -5,10 +5,15 @@ def main():
 
     print("📚 Kütüphane Otomasyonu\n")
     print("1- Kitap ekle")
+    print("2- Kitaplari Listele")
+
     secim = input("Seçim: ")
 
     if secim == "1":
         service.add_book()
+    elif secim == "2":
+        service.list_books()
+
 
 if __name__ == "__main__":
     main()
--- src/book_service.py
+++ src/book_service.py
@@ -14,3 +14,19 @@ class BookService:
             f.write(str(book) + "\n")
 
         print("Kitap başariyla kaydedildi ✔")
+
+    def list_books(self):
+        try:
+            with open("data/books.txt", "r", encoding="utf-8") as f:
+                lines = f.readlines()
+
+            if len(lines) == 0:
+                print(" Kayitli kitap bulunamadi.")
+                return
+            
+            print("\n Kaydedilmiş Kitaplar:")
+            for line in lines:
+                print(line.strip())
+
+        except FileNotFoundError:
+            print("books.txt dosyasi bulunamadi!")
